215c0d1ab25c573eb59003a955dec86e7fba6f4d
Raimokarmayuri <mayuriraimokar@gmail.com>
2024-06-18 15:08:45 +0530

    chnage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/stable'
 import 'core-js'
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
